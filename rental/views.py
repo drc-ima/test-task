41550d8d35f05479be7dfa2bae7cb8482210c1b3
Emmanuel Ofosu <emmanuelofosu472@gmail.com>
2022-05-12 16:03:07 +0000

    init commit

--- rental/views.py
+++ rental/views.py
@@ -38,36 +38,3 @@ class AddReservation(CreateView):
     form_class = ReservationForm
     success_url = reverse_lazy('reservations')
 
-
-# add a reservation to a rental using django forms through rental details page
-def rental_details(request, id):
-
-    rental = None
-
-    try:
-        rental = Rental.objects.get(id=id)
-    except Rental.DoesNotExist:
-        raise Http404
-
-    form = ReservationForm()
-
-    reservations = Reservation.objects.filter(rental=rental)
-
-    context = {
-        'object': rental,
-        'form': form,
-        'object_list': reservations
-    }
-
-    # post request to add reservation to rental
-    if request.method == 'POST':
-        form = ReservationForm(request.POST)
-
-        form.instance.rental = rental
-
-        form.save()
-
-        # return to the details page
-        return redirect('rental-details', id)
-
-    return render(request, 'rental/details.html', context)
